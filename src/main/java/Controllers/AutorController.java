0ca70d7fe58e39d436eb659a8a6b2efb64cbc897
Marcos Maues <yboostx@gmail.com>
2025-05-05 15:06:48 -0300

    "att"

---
+++ src/main/java/Controllers/AutorController.java
@@ -0,0 +1,36 @@
+package Controllers;
+
+import entities.Autors;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class AutorController {
+    List<Autors> autors;
+
+    public AutorController() {
+        autors = new ArrayList<Autors>();
+    }
+
+    public List<Autors> getAutors() {
+        return autors;
+    }
+
+    public void setAutors(List<Autors> autors) {
+        this.autors = autors;
+    }
+
+    public void cadastrarAutor(Autors autor) {
+        autors.add(autor);
+    }
+
+    public Optional<Autors> getAutor(String nomeAutor) {
+        for (Autors autor : autors) {
+            if (autor.getNome().equals(nomeAutor)) {
+                return Optional.of(autor);
+            }
+        }
+        return Optional.empty();
+    }
+}
---
+++ src/main/java/Controllers/CategoriaController.java
@@ -0,0 +1,40 @@
+package Controllers;
+
+import entities.Categoria;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class CategoriaController {
+    List<Categoria> listaCategoria;
+
+    public CategoriaController() {
+        listaCategoria = new ArrayList<>();
+    }
+
+    public List<Categoria> getListaCategoria() {
+        return listaCategoria;
+    }
+
+    public void setListaCategoria(List<Categoria> listaCategoria) {
+        this.listaCategoria = listaCategoria;
+    }
+
+    public void cadastrarCategoria(Categoria categoria) {
+        listaCategoria.add(categoria);
+    }
+
+    public void excluirCategoria(Categoria categoria) {
+        listaCategoria.remove(categoria);
+    }
+
+    public Optional<Categoria> buscarCategoriaPorNome(String nome) {
+        for (Categoria categoria : listaCategoria) {
+            if(categoria.getNomeCategoria().equals(nome)) {
+                return Optional.of(categoria);
+            }
+        }
+        return Optional.empty();
+    }
+}
