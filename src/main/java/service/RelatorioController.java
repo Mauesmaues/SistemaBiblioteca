1b45a120954bc6843f946de3dc85b5f009fc55ab
GabrieLaureanoUP <gabriel.silva0@cs.up.edu.br>
2025-05-13 17:38:37 -0300

    Emprestimo

--- src/main/java/service/RelatorioController.java
+++ src/main/java/service/RelatorioController.java
@@ -1,40 +1,38 @@
 package service;
 
-import controllers.EmprestimoController;
-import enums.StatusEmprestimo;
-
 import java.util.List;
 
+import controllers.EmprestimoController;
+import enums.StatusEmprestimo;
+import model.Emprestimo;
 
 public class RelatorioController {
 
-     public static List<String> listarEmprestimosAtivos() {
+    public static List<Emprestimo> listarEmprestimosAtivos() {
         return EmprestimoController.listarEmprestimos().stream()
-            .filter(emprestimo -> emprestimo.getStatusEmprestimo() == StatusEmprestimo.EMPRESTADO || emprestimo.getStatusEmprestimo() == StatusEmprestimo.ATRASADO)
-            .map(emprestimo -> emprestimo.getLivro().getTitulo())
-            .toList();
+                .filter(emprestimo -> emprestimo.getStatusEmprestimo() == StatusEmprestimo.EMPRESTADO || emprestimo.getStatusEmprestimo() == StatusEmprestimo.ATRASADO)
+                .toList();
     }
 
     public static List<String> usuariosComDevolucoesAtrasadas() {
         return EmprestimoController.listarEmprestimos().stream()
-            .filter(emprestimo -> emprestimo.getStatusEmprestimo() == StatusEmprestimo.ATRASADO)
-            .map(emprestimo -> emprestimo.getUsuario().getNome())
-            .toList();
+                .filter(emprestimo -> emprestimo.getStatusEmprestimo() == StatusEmprestimo.ATRASADO)
+                .map(emprestimo -> emprestimo.getUsuario().getNome())
+                .toList();
     }
 
     public static List<String> listarLivrosPopulares() {
-    return EmprestimoController.listarEmprestimos().stream()
-            .collect(java.util.stream.Collectors.toMap(
-                    e -> e.getLivro().getTitulo(),      
-                    e -> 1,                             
-                    Integer::sum                        
-                    ))
-            .entrySet()
-            .stream()
-            .sorted((a, b) -> b.getValue().compareTo(a.getValue())) 
-            .map(entry -> entry.getKey() + " - " + entry.getValue() + " empréstimos")
-            .toList();
+        return EmprestimoController.listarEmprestimos().stream()
+                .collect(java.util.stream.Collectors.toMap(
+                        e -> e.getLivro().getTitulo(),
+                        e -> 1,
+                        Integer::sum
+                ))
+                .entrySet()
+                .stream()
+                .sorted((a, b) -> b.getValue().compareTo(a.getValue()))
+                .map(entry -> entry.getKey() + " - " + entry.getValue() + " empréstimos")
+                .toList();
     }
 
 }
-
